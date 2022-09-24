8ebff41a161ff05e3f5eef9bb8762f55b3c5d574
codebaya <beauglory@gmail.com>
2022-09-24 11:51:08 -0400

    saving

--- rooms/apps.py
+++ rooms/apps.py
@@ -1,11 +1,6 @@
 from django.apps import AppConfig
 
 
-class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
-
-
 class RoomsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'rooms'
