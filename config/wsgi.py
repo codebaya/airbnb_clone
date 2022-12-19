9a98c7f94e84008d3658fd04fa4b495dafc03bbd
codebaya <beauglory@gmail.com>
2022-12-19 17:28:58 -0500

    renderfinal

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,16 +8,15 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
 # path = '/Users/glory/Documents/python_Django/airbnb_clone_backend'
 # if path not in sys.path:
 #     sys.path.append(path)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
-os.environ('DJANGO_SETTINGS_MODULE', 'config.settings')
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# sys.path.append(BASE_DIR)
+# os.environ('DJANGO_SETTINGS_MODULE', 'config.settings')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
