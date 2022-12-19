562c57bb11a3fd165a1ff98bc2197bdbb95604f7
codebaya <beauglory@gmail.com>
2022-12-19 17:03:40 -0500

    renderfii

--- config/wsgi.py
+++ config/wsgi.py
@@ -12,9 +12,12 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-path = '/Users/glory/Documents/python_Django/airbnb_clone_backend'
-if path not in sys.path:
-    sys.path.append(path)
+# path = '/Users/glory/Documents/python_Django/airbnb_clone_backend'
+# if path not in sys.path:
+#     sys.path.append(path)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ('DJANGO_SETTINGS_MODULE', 'config.settings')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
